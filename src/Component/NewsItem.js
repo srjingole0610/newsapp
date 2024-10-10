8fe3befe1dbe21be3cb8d65ecf178a9a37855a99
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2024-10-10 22:13:04 +0530

    Code Cleanup and added component

---
+++ src/Component/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+       <h3> This is a newsItem Component</h3>
+      </div>
+    )
+  }
+}
+
+export default NewsItem

6209885db1b4a2649744c6734fd6c2792075c21a
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2024-10-11 12:10:17 +0530

    Added loader and variable pageSize

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import Navbar from './Component/Navbar';
 import News from './Component/News';
 
  class App extends Component {
+/**
+ * Renders the App component.
+ *
+ * This component renders a container with a Navbar and a News component. The
+ * News component is rendered with a pageSize of 5.
+ *
+ * @return {JSX.Element} The rendered component.
+ */
   render() {
    
     return (
       <div>
         <Navbar/>
-        <News/>
+        <News pageSize = {5}/>
       </div>
     )
   }}
